e655a7b38c31f59a9608fd8fc4f1726e27d0474c
yueny09 <yueny09@163.com>
2016-11-15 14:47:38 +0800

    metrics demo

---
+++ metrics/src/main/java/com/yueny/demo/metrics/service/IDataPrecipitationService.java
@@ -0,0 +1,35 @@
+package com.yueny.demo.metrics.service;
+
+import java.util.List;
+
+import com.yueny.demo.metrics.bo.ModifyDemoBo;
+import com.yueny.superclub.util.common.enums.YesNoType;
+
+/**
+ * @author yueny09 <deep_blue_yang@163.com>
+ *
+ * @DATE 2016年8月5日 上午9:56:50
+ *
+ */
+public interface IDataPrecipitationService {
+	/**
+	 * 根据主键查询
+	 */
+	ModifyDemoBo findById(Long primaryId);
+
+	/**
+	 * 新增
+	 */
+	Long insert(ModifyDemoBo data);
+
+	/**
+	 * @return
+	 */
+	List<ModifyDemoBo> queryAll();
+
+	/**
+	 * 根据类型查询数据
+	 */
+	List<ModifyDemoBo> queryByType(YesNoType type);
+
+}
